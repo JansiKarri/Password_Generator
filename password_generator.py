4ff73d4446ba1973ed025df00f836b8b063701e3
JansiKarri <140739586+JansiKarri@users.noreply.github.com>
2023-07-27 22:32:00 +0530

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,12 +1,12 @@
 import random
 import string
 
-def generate_random_password(length):
-    characters = string.ascii_letters + string.digits + string.punctuation
+def generate_password(length=8):
+    characters = string.ascii_lowercase
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-if __name__ == "__main__":
-    password_length = 6  # Change this value to set the desired password length
-    random_password = generate_random_password(password_length)
-    print("Generated Random Password:", random_password)
+if _name_ == "_main_":
+    password_length = 8  # You can change this to your desired password length
+    generated_password = generate_password(password_length)
+    print("Generated Password:", generated_password)
